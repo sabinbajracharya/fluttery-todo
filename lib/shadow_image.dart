31ef7dabe07e0679655e61f89d52f893c462473c
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2019-02-13 02:34:24 +0545

    Decrease image size and reduce blur radius

--- lib/shadow_image.dart
+++ lib/shadow_image.dart
@@ -10,7 +10,7 @@ class ShadowImage extends StatelessWidget {
         boxShadow: [
           BoxShadow(
             color: Colors.black.withOpacity(0.6),
-            blurRadius: 20.0, // has the effect of softening the shadow
+            blurRadius: 25.0, // has the effect of softening the shadow
             spreadRadius: 0.0, // has the effect of extending the shadow
             offset: Offset(
               0.0, // horizontal, move right 10
@@ -22,8 +22,8 @@ class ShadowImage extends StatelessWidget {
       child: ClipOval(
         child: Image.network(
           'https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg',
-          height: 60,
-          width: 60,
+          height: 52,
+          width: 52,
           fit: BoxFit.cover,
         ),
       ),
