371120d4795dc32b24195b766ef92a11dcb532ac
Stefan Zindl <zindlsn@studi.informatik.uni-stuttgart.de>
2022-02-11 18:39:39 +0100

    #10 Migrate project to Android embedding v2:
    https://github.com/flutter/flutter/wiki/Upgrading-pre-1.12-Android-projects

--- android/app/src/main/java/com/example/todo/MainActivity.java
+++ android/app/src/main/java/com/example/todo/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.todo;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity
 
 public class MainActivity extends FlutterActivity {
   @Override
